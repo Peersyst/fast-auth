ed29f773c52411726dbd9b5a5d2fa7d1f9ed7088
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-03-25 10:17:21 +0100

    chore: linting packages and adding new folders to `pnpm-workspace`

--- packages/auth/test/index.test.ts
+++ packages/auth/test/index.test.ts
@@ -1,5 +1,5 @@
-describe('Auth SDK', () => {
-    it('should pass a dummy test', () => {
-        expect(true).toBe(true);
-    });
+describe("Auth SDK", () => {
+  it("should pass a dummy test", () => {
+    expect(true).toBe(true);
+  });
 });
