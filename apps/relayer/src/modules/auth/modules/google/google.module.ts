788c34a46ea1790978d599c1fac501ed354bbd93
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-03-26 18:54:49 +0100

    feat(auth0-poc): auth0 poc

--- apps/relayer/src/modules/auth/modules/google/google.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { GoogleAuthService } from "./google.service";
-
-@Module({
-    providers: [GoogleAuthService],
-    exports: [GoogleAuthService],
-})
-export class GoogleModule {}
--- apps/relayer/src/modules/firebase/firebase.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { FirebaseService } from "./firebase.service";
-import { FirebaseAuthService } from "./firebase.auth.service";
-
-@Module({
-    providers: [FirebaseService, FirebaseAuthService],
-    exports: [FirebaseService],
-})
-export class FirebaseModule {}
