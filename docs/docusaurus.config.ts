ce68529f19975ef7007e2a461049b23545712cf1
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-05-29 12:08:20 +0200

    feat(docs): docs setup

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,178 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: "FastAuth",
+    favicon: "img/favicon.ico",
+
+    // Set the production url of your site here
+    url: "https://peersyst.github.io",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/fast-auth",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "Peersyst", // Usually your GitHub org/user name.
+    projectName: "fast-auth", // Usually your repo name.
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+
+    plugins: [
+        [
+            "@easyops-cn/docusaurus-search-local",
+            {
+                hashed: true,
+                docsRouteBasePath: "/docs",
+            },
+        ],
+    ],
+
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "Fast Auth",
+            logo: {
+                alt: "FastAuth Logo",
+                src: "img/near-logo.webp",
+            },
+            items: [
+                {
+                    type: "docSidebar",
+                    sidebarId: "conceptsSidebar",
+                    position: "left",
+                    label: "Concepts",
+                },
+                {
+                    type: "docSidebar",
+                    sidebarId: "integrationsSidebar",
+                    position: "left",
+                    label: "Integrations",
+                },
+                {
+                    type: "dropdown",
+                    label: "SDKs",
+                    position: "left",
+                    items: [
+                        {
+                            label: "Browser",
+                            to: "/docs/sdk/browser",
+                        },
+                    ],
+                },
+                {
+                    href: "https://github.com/Peersyst/fast-auth",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Concepts",
+                    items: [
+                        {
+                            label: "Overview",
+                            to: "/docs/concepts/overview",
+                        },
+                        {
+                            label: "Architecture",
+                            to: "/docs/concepts/architecture",
+                        },
+                        {
+                            label: "Auth0",
+                            to: "/docs/concepts/auth0",
+                        },
+                        {
+                            label: "Custom Backend",
+                            to: "/docs/concepts/custom-backend",
+                        },
+                    ],
+                },
+                {
+                    title: "Guides",
+                    items: [
+                        {
+                            label: "Getting Started",
+                            to: "/docs/integrations/getting-started",
+                        },
+                        {
+                            label: "Auth0",
+                            to: "/docs/integrations/auth0",
+                        },
+                        {
+                            label: "Custom Backend",
+                            to: "/docs/integrations/custom-backend",
+                        },
+                        {
+                            label: "DApp",
+                            to: "/docs/integrations/dapp",
+                        },
+                    ],
+                },
+                {
+                    title: "SDKs",
+                    items: [
+                        {
+                            label: "Browser",
+                            to: "/docs/sdk/browser",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Peersyst, Inc.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docs/sidebars.ts
@@ -0,0 +1,21 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+const sidebars: SidebarsConfig = {
+    conceptsSidebar: ["concepts/overview", "concepts/architecture", "concepts/auth0", "concepts/custom-backend"],
+    integrationsSidebar: ["integrations/getting-started", "integrations/auth0", "integrations/custom-backend", "integrations/dapp"],
+    sdkSidebar: ["sdk/browser"],
+};
+
+export default sidebars;
