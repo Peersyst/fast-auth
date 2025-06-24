a8f16c5f5cccd86025b3744cfe954bbe221e0e46
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2025-06-24 16:45:27 +0200

    refactor(docs): wip pages

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,17 +1,23 @@
 import type { ReactNode } from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return <></>;
-}
+import { HeroSection } from "../components/HeroSection";
+import HomepageFeatures from "../components/HomepageFeatures";
 
 export default function Home(): ReactNode {
     const { siteConfig } = useDocusaurusContext();
     return (
         <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-            <HomepageHeader />
-            <main></main>
+            <main>
+                <HeroSection
+                    title="FastAuth"
+                    description="FastAuth is a NEAR Protocol-based authentication system that enables secure transaction signing through Multi-Party Computation (MPC) and JWT-based verification."
+                    imageSrc="img/near-logo.webp"
+                    imageAlt="FastAuth Logo"
+                >
+                    <HomepageFeatures />
+                </HeroSection>
+            </main>
         </Layout>
     );
 }
