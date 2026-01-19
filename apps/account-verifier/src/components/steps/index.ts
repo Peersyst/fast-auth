f025b40490144eed51d2b2ac486c20026664a786
AdriaCarrera <adria.cm95@gmail.com>
2026-01-19 17:18:45 +0100

    remove account verifier

--- apps/account-verifier/src/components/steps/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as LoginStep } from './LoginStep';
-export { default as CreateAccountStep } from './CreateAccountStep';
-export { default as RequestTransactionStep } from './RequestTransactionStep';
-export { default as SignTransactionStep } from './SignTransactionStep';
-export { default as BroadcastStep } from './BroadcastStep';
