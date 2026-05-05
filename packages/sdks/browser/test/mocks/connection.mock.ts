b1c46e4685759c5a3ddfaac1a136b2fa7b0791b4
AdriaCarrera <adria.cm95@gmail.com>
2026-05-05 16:11:18 +0200

    fix: connection.mock.ts

--- packages/sdks/browser/test/mocks/connection.mock.ts
+++ packages/sdks/browser/test/mocks/connection.mock.ts
@@ -1,15 +1,40 @@
 import { createMock, MethodMock } from "@shared/test";
-import { Connection } from "near-api-js";
+import { Connection, Signer } from "near-api-js";
 import { Provider } from "near-api-js/lib/providers";
 
 const ProviderMock = createMock<Provider>({
+    accessKeyChanges: new MethodMock("mockReturnValue", undefined),
+    accountChanges: new MethodMock("mockReturnValue", undefined),
+    block: new MethodMock("mockReturnValue", undefined),
+    blockChanges: new MethodMock("mockReturnValue", undefined),
+    chunk: new MethodMock("mockReturnValue", undefined),
+    contractCodeChanges: new MethodMock("mockReturnValue", undefined),
+    contractStateChanges: new MethodMock("mockReturnValue", undefined),
+    experimental_protocolConfig: new MethodMock("mockReturnValue", undefined),
+    gasPrice: new MethodMock("mockReturnValue", undefined),
+    lightClientProof: new MethodMock("mockReturnValue", undefined),
+    nextLightClientBlock: new MethodMock("mockReturnValue", undefined),
+    sendTransactionAsync: new MethodMock("mockReturnValue", undefined),
+    sendTransactionUntil: new MethodMock("mockReturnValue", undefined),
+    singleAccessKeyChanges: new MethodMock("mockReturnValue", undefined),
+    status: new MethodMock("mockReturnValue", undefined),
+    txStatus: new MethodMock("mockReturnValue", undefined),
+    txStatusReceipts: new MethodMock("mockReturnValue", undefined),
+    validators: new MethodMock("mockReturnValue", undefined),
     query: new MethodMock("mockReturnValue", undefined),
     sendTransaction: new MethodMock("mockReturnValue", undefined),
 });
 
+export const SignerMock = createMock<Signer>({
+    createKey: new MethodMock("mockReturnValue", undefined),
+    getPublicKey: new MethodMock("mockReturnValue", undefined),
+    signMessage: new MethodMock("mockReturnValue", undefined),
+});
+
 export const ConnectionMock = createMock<Connection>({
+    jsvmAccountId: "",
     provider: new ProviderMock(),
-    signer: new MethodMock("mockReturnValue", undefined),
+    signer: new SignerMock(),
     getConnection: new MethodMock("mockReturnValue", undefined),
     networkId: "",
 });
